3cce551b0ca707de0c5e20ac40e33eac782e3f54
Titenq <titenq@gmail.com>
2020-03-08 10:56:20 -0300

    Add Helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const helmet = require('helmet');
 const express = require('express');
 
-const app = express();
 const port = process.env.PORT || 3000;
+const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
