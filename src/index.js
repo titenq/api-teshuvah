792122dd0120a4f32347cf4fb710bcdd9ada3673
Titenq <titenq@gmail.com>
2020-03-13 07:25:38 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const cors = require('cors');
 const helmet = require('helmet');
 const express = require('express');
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
